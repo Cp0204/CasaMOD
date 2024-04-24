99e8d978598b7cd6ba8a7224d9823b185026f646
Cp0204 <Cp0204@qq.com>
2024-04-24 15:03:36 +0800

    Adds blur and brightness effects to bing-wallpaper

--- app/mod/bing-wallpaper/mod.js
+++ app/mod/bing-wallpaper/mod.js
@@ -2,6 +2,7 @@ const intervalId = setInterval(() => {
   const element = document.getElementById('background');
   if (element) {
     element.style.backgroundImage = 'url("https://bing.img.run/1920x1080.php")';
+    element.style.filter = 'blur(5px) brightness(0.8)'; // 毛玻璃和压暗效果
     clearInterval(intervalId);
   }
 }, 100);
